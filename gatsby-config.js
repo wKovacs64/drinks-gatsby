bf24063d4f013654f3ccd409b0ee77fb2b0f2374
Justin Hall <justin.r.hall@gmail.com>
2018-07-22 14:18:19 -0600

    Bring in Typography.js with Moraga theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,14 @@ module.exports = {
   siteMetadata: {
     title: 'Drinks',
   },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-emotion'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-emotion',
+    {
+      resolve: 'gatsby-plugin-typography',
+      options: {
+        pathToConfigModule: 'src/utils/typography.js',
+      },
+    },
+  ],
 };
---
+++ src/utils/typography.js
@@ -0,0 +1,6 @@
+import Typography from 'typography';
+import moraga from 'typography-theme-moraga';
+
+const typography = new Typography({ ...moraga, headerWeight: 400 });
+
+export default typography;
