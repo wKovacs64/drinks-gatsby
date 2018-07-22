6fae7ec3cb9d8bafa243eb58690f0a69beb019bc
Justin Hall <justin.r.hall@gmail.com>
2018-07-21 20:40:05 -0600

    Rename component files to PascalCase

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { StaticQuery, graphql } from 'gatsby';
-import Header from './header';
+import Header from './Header';
 
 const Layout = ({ children /* , data */ }) => (
   <StaticQuery
