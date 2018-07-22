74a84bc3b1b0cf0c1481914973f522990dd5fe63
Justin Hall <justin.r.hall@gmail.com>
2018-07-22 16:13:40 -0600

    Enable offline and manifest plugins for PWA (#3)

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,5 +11,18 @@ module.exports = {
         pathToConfigModule: 'src/utils/typography.js',
       },
     },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Drinks',
+        short_name: 'Drinks',
+        start_url: '/',
+        background_color: '#f4f4f4',
+        theme_color: '#137752',
+        display: 'minimal-ui',
+        icon: 'src/images/icon.png',
+      },
+    },
+    'gatsby-plugin-offline',
   ],
 };
