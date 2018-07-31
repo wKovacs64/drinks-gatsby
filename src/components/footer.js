b50f1df1f81d887e03f69050866ea2e29bfc598f
Justin Hall <justin.r.hall@gmail.com>
2018-07-30 19:20:16 -0600

    Add footer links and additional styles

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,21 @@
 import React from 'react';
-import { css } from 'react-emotion';
+import styled, { css } from 'react-emotion';
 import mq from '../utils/mq';
+
+const FooterLink = styled.a`
+  padding-bottom: 0.25rem;
+  color: currentColor;
+  text-decoration: none;
+  border-bottom: 1px solid #cccccc;
+  box-shadow: inset 0 -2px 0 0 #cccccc;
+  transition: color 0.3s ease, border-color 0.3s ease, box-shadow 0.3s ease;
+
+  &:hover {
+    color: #f4f4f4;
+    border-color: #f4f4f4;
+    box-shadow: inset 0 -2px 0 0 #f4f4f4;
+  }
+`;
 
 const Footer = () => (
   <footer
@@ -8,12 +23,37 @@ const Footer = () => (
       background-color: #111111;
       color: #eeeeee;
       padding: 1rem;
+      font-size: 1rem;
+      ${mq.sm(css`
+        font-size: 1.25rem;
+      `)};
       ${mq.md(css`
         padding: 2rem;
       `)};
     `}
   >
-    <span>Built with ♥ using Gatsby, Contentful, and Emotion</span>
+    <span>
+      Built with{' '}
+      <span
+        className={css`
+          text-decoration: line-through;
+        `}
+      >
+        ♥
+      </span>{' '}
+      🥃 using{' '}
+      <FooterLink href="https://gatsbyjs.org" rel="noopener noreferrer">
+        Gatsby
+      </FooterLink>
+      ,{' '}
+      <FooterLink href="https://www.contentful.com/" rel="noopener noreferrer">
+        Contentful
+      </FooterLink>
+      , and{' '}
+      <FooterLink href="https://emotion.sh/" rel="noopener noreferrer">
+        Emotion
+      </FooterLink>
+    </span>
   </footer>
 );
 
