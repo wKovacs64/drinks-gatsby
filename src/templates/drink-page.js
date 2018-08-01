1e6672769ad4f1c200981a9c18e6f7e163724a53
Justin Hall <justin.r.hall@gmail.com>
2018-07-31 20:54:05 -0600

    Add manual breadcrumb to DrinkPage

--- src/templates/drink-page.js
+++ src/templates/drink-page.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
-import { graphql } from 'gatsby';
-import { css } from 'react-emotion';
+import { Link, graphql } from 'gatsby';
+import styled, { css } from 'react-emotion';
 import Layout from '../components/layout';
 import Main from '../components/main';
 import Glass from '../components/glass';
 import DrinkSummary from '../components/drink-summary';
 import DrinkDetails from '../components/drink-details';
 import mq from '../utils/mq';
+
+const NavLink = styled(Link)`
+  padding-bottom: 0.25rem;
+  color: currentColor;
+  text-decoration: none;
+  border-bottom: 1px solid #cccccc;
+  box-shadow: inset 0 -2px 0 0 #cccccc;
+  transition: color 0.3s ease, border-color 0.3s ease, box-shadow 0.3s ease;
+
+  &:hover {
+    color: #f4f4f4;
+    border-color: #f4f4f4;
+    box-shadow: inset 0 -2px 0 0 #f4f4f4;
+  }
+`;
 
 const DrinkPage = ({ data: { drink } }) => (
   <Layout>
@@ -32,8 +47,35 @@ const DrinkPage = ({ data: { drink } }) => (
         `)};
       `}
     >
+      <div
+        className={css`
+          display: none;
+          ${mq.xl(css`
+            display: block;
+            color: #eeeeee;
+            margin: 2rem 0;
+            width: 70rem;
+          `)};
+        `}
+      >
+        <NavLink to="../">All Drinks</NavLink>
+        <span
+          className={css`
+            margin: 0 1rem;
+          `}
+        >
+          ⇒
+        </span>
+        {drink.title}
+      </div>
       <Glass
         className={css`
+          ${mq.lg(css`
+            margin: 2rem 0;
+          `)};
+          ${mq.xl(css`
+            margin: 0 0 4rem 0;
+          `)};
           ${mq.lg(css`
             border-width: 4px 0;
           `)};
