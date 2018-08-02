d693c50fa70d81731f2aea6e32cc704af140980c
Justin Hall <justin.r.hall@gmail.com>
2018-08-01 18:28:36 -0600

    Remove bottom border from unfocused breadcrumb links

--- src/templates/drink-page.js
+++ src/templates/drink-page.js
@@ -14,8 +14,6 @@ const NavLink = styled(Link)`
   padding-bottom: 0.25rem;
   color: currentColor;
   text-decoration: none;
-  border-bottom: 1px solid #cccccc;
-  box-shadow: inset 0 -2px 0 0 #cccccc;
   transition: color 0.3s ease, border-color 0.3s ease, box-shadow 0.3s ease;
 
   &:hover {
