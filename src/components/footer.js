45bc148dcd8343add99da50cf9a23cfeabed5723
Justin Hall <justin.r.hall@gmail.com>
2018-08-01 18:27:03 -0600

    Remove box-shadow from unfocused footer links

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,7 +7,6 @@ const FooterLink = styled.a`
   color: currentColor;
   text-decoration: none;
   border-bottom: 1px solid #cccccc;
-  box-shadow: inset 0 -2px 0 0 #cccccc;
   transition: color 0.3s ease, border-color 0.3s ease, box-shadow 0.3s ease;
 
   &:hover {
