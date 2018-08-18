51f48398ce1118e83ea91ef0493797fe9c2687cc
Justin Hall <justin.r.hall@gmail.com>
2018-08-18 16:49:14 -0600

    Hide live search when JavaScript is disabled

--- src/components/header.js
+++ src/components/header.js
@@ -8,9 +8,14 @@ import mq from '../utils/mq';
 
 class Header extends Component {
   state = {
+    jsEnabled: false,
     searchTerm: '',
     showSearch: false,
   };
+
+  componentDidMount() {
+    this.setState({ jsEnabled: true });
+  }
 
   clearSearchTerm = () => {
     this.setState({ searchTerm: '' }, () => {
@@ -36,7 +41,7 @@ class Header extends Component {
 
   render() {
     const { siteTitle, withSearch } = this.props;
-    const { searchTerm, showSearch } = this.state;
+    const { jsEnabled, searchTerm, showSearch } = this.state;
 
     return (
       <IconContext.Provider
@@ -79,61 +84,66 @@ class Header extends Component {
               {siteTitle}
             </Link>
           </h1>
-          {withSearch && (
-            <div>
-              <input
-                id="search-input"
-                name="searchInput"
-                aria-label="Search Term"
-                type="text"
-                placeholder="Search"
-                autoComplete="off"
-                autoCorrect="off"
-                autoCapitalize="off"
-                spellCheck={false}
-                value={searchTerm}
-                onChange={this.handleSearchTermChange}
-                onKeyDown={this.handleSearchTermKeydown}
-                className={css`
-                  color: #f4f4f4;
-                  background-color: transparent;
-                  border-width: 0 0 1px;
-                  padding: 0 0 0.25rem 0;
-                  width: ${showSearch ? '40vw' : '0'};
-                  max-width: 10rem;
-                  transition: width 0.2s ease-in-out;
-                  ${mq.sm(css`
-                    width: ${showSearch ? '10rem' : '0'};
-                    max-width: unset;
-                  `)};
-                  ${mq.md(css`
-                    padding: 0 0 0.5rem 0;
-                    width: ${showSearch ? '12rem' : '0'};
-                  `)};
-                `}
-              />
-              <button
-                className={css`
-                  cursor: pointer;
-                  color: #cccccc;
-                  background-color: transparent;
-                  border: none;
-                  transition: color 0.3s ease;
-                  &:hover {
+          {jsEnabled &&
+            withSearch && (
+              <div>
+                <input
+                  id="search-input"
+                  name="searchInput"
+                  aria-label="Search Term"
+                  type="text"
+                  placeholder="Search"
+                  autoComplete="off"
+                  autoCorrect="off"
+                  autoCapitalize="off"
+                  spellCheck={false}
+                  value={searchTerm}
+                  onChange={this.handleSearchTermChange}
+                  onKeyDown={this.handleSearchTermKeydown}
+                  className={css`
                     color: #f4f4f4;
-                  }
-                  ${mq.md(css`
-                    padding: 1rem;
-                  `)};
-                `}
-                aria-label="Toggle Search"
-                type="button"
-                onClick={this.toggleSearch}
-              >
-                {showSearch ? <MdZoomOut size={32} /> : <MdSearch size={32} />}
-              </button>
-            </div>
-          )}
+                    background-color: transparent;
+                    border-width: 0 0 1px;
+                    padding: 0 0 0.25rem 0;
+                    width: ${showSearch ? '40vw' : '0'};
+                    max-width: 10rem;
+                    transition: width 0.2s ease-in-out;
+                    ${mq.sm(css`
+                      width: ${showSearch ? '10rem' : '0'};
+                      max-width: unset;
+                    `)};
+                    ${mq.md(css`
+                      padding: 0 0 0.5rem 0;
+                      width: ${showSearch ? '12rem' : '0'};
+                    `)};
+                  `}
+                />
+                <button
+                  className={css`
+                    cursor: pointer;
+                    color: #cccccc;
+                    background-color: transparent;
+                    border: none;
+                    transition: color 0.3s ease;
+                    &:hover {
+                      color: #f4f4f4;
+                    }
+                    ${mq.md(css`
+                      padding: 1rem;
+                    `)};
+                  `}
+                  aria-label="Toggle Search"
+                  type="button"
+                  onClick={this.toggleSearch}
+                >
+                  {showSearch ? (
+                    <MdZoomOut size={32} />
+                  ) : (
+                    <MdSearch size={32} />
+                  )}
+                </button>
+              </div>
+            )}
         </header>
       </IconContext.Provider>
     );
