6bbf58deb08d87198f4aaac7a3a5a06cc4fb6c4f
Justin Hall <justin.r.hall@gmail.com>
2018-10-30 12:14:36 -0600

    Convert to hooks

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import '@wkovacs64/normalize.css';
 import 'typeface-source-sans-pro';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { IconContext } from 'react-icons';
 import Helmet from 'react-helmet';
@@ -13,125 +13,120 @@ import Main from './main';
 import Footer from './footer';
 import mq from '../utils/mq';
 
-class Layout extends Component {
-  state = { feedbackOpen: false };
+function Layout({ children, onSearchTermChange, withSearch }) {
+  const [feedbackOpen, setFeedbackOpen] = useState(false);
 
-  handleFeedbackToggle = () => {
-    this.setState(({ feedbackOpen }) => ({ feedbackOpen: !feedbackOpen }));
-  };
+  function handleFeedbackToggle() {
+    setFeedbackOpen(open => !open);
+  }
 
-  render() {
-    const { children, onSearchTermChange, withSearch } = this.props;
-    const { feedbackOpen } = this.state;
-
-    return (
-      <StaticQuery
-        query={graphql`
-          {
-            site {
-              siteMetadata {
-                title
-                buildInfo {
-                  commit
-                  version
-                }
+  return (
+    <StaticQuery
+      query={graphql`
+        {
+          site {
+            siteMetadata {
+              title
+              buildInfo {
+                commit
+                version
               }
             }
           }
-        `}
-      >
-        {({ site: { siteMetadata } }) => (
-          <ClassNames>
-            {({ css: classNameFromCss }) => (
-              <IconContext.Provider
-                value={{
-                  className: classNameFromCss`
-                    vertical-align: middle;
-                  `,
-                }}
+        }
+      `}
+    >
+      {({ site: { siteMetadata } }) => (
+        <ClassNames>
+          {({ css: classNameFromCss }) => (
+            <IconContext.Provider
+              value={{
+                className: classNameFromCss`
+                  vertical-align: middle;
+                `,
+              }}
+            >
+              <Global
+                styles={css`
+                  @font-face {
+                    font-family: 'Source Sans Pro';
+                    src: local('Source Sans Pro');
+                  }
+                  html {
+                    background-color: #242424;
+                    overflow-y: scroll;
+                  }
+                  body {
+                    font-family: 'Source Sans Pro', -apple-system,
+                      BlinkMacSystemFont, 'avenir next', avenir,
+                      'helvetica neue', helvetica, ubuntu, roboto, noto,
+                      'segoe ui', arial, sans-serif;
+                    font-weight: 300;
+                  }
+                `}
+              />
+              <Helmet>
+                <html
+                  lang="en"
+                  data-commit={siteMetadata.buildInfo.commit}
+                  data-version={siteMetadata.buildInfo.version}
+                />
+              </Helmet>
+              <FeedbackDialog
+                isOpen={feedbackOpen}
+                onDismiss={handleFeedbackToggle}
+              />
+              <div
+                css={css`
+                  display: flex;
+                  flex-direction: column;
+                  min-height: 100vh;
+                  position: relative;
+                  &::after {
+                    content: '';
+                    background-image: url(${backgroundImage});
+                    background-repeat: no-repeat;
+                    background-size: cover;
+                    background-attachment: fixed;
+                    background-position: center;
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    bottom: 0;
+                    right: 0;
+                    z-index: -1;
+                  }
+                `}
               >
-                <Global
-                  styles={css`
-                    @font-face {
-                      font-family: 'Source Sans Pro';
-                      src: local('Source Sans Pro');
-                    }
-                    html {
-                      background-color: #242424;
-                      overflow-y: scroll;
-                    }
-                    body {
-                      font-family: 'Source Sans Pro', -apple-system,
-                        BlinkMacSystemFont, 'avenir next', avenir,
-                        'helvetica neue', helvetica, ubuntu, roboto, noto,
-                        'segoe ui', arial, sans-serif;
-                      font-weight: 300;
-                    }
-                  `}
+                <Header
+                  siteTitle={siteMetadata.title}
+                  onSearchTermChange={onSearchTermChange}
+                  withSearch={withSearch}
                 />
-                <Helmet>
-                  <html
-                    lang="en"
-                    data-commit={siteMetadata.buildInfo.commit}
-                    data-version={siteMetadata.buildInfo.version}
-                  />
-                </Helmet>
-                <FeedbackDialog
-                  isOpen={feedbackOpen}
-                  onDismiss={this.handleFeedbackToggle}
-                />
-                <div
-                  css={css`
-                    display: flex;
-                    flex-direction: column;
-                    min-height: 100vh;
-                    position: relative;
-                    &::after {
-                      content: '';
-                      background-image: url(${backgroundImage});
-                      background-repeat: no-repeat;
-                      background-size: cover;
-                      background-attachment: fixed;
-                      background-position: center;
-                      position: absolute;
-                      top: 0;
-                      left: 0;
-                      bottom: 0;
-                      right: 0;
-                      z-index: -1;
-                    }
-                  `}
-                >
-                  <Header
-                    siteTitle={siteMetadata.title}
-                    onSearchTermChange={onSearchTermChange}
-                    withSearch={withSearch}
-                  />
-                  <noscript>
-                    <p
-                      css={css`
-                        color: #f4f4f4;
-                        margin-bottom: 0;
-                        padding: 1rem;
-                        ${mq.sm(css`
-                          padding: 0;
-                          text-align: center;
-                        `)}
-                      `}
-                    >
-                      Please enable JavaScript for full site functionality.
-                    </p>
-                  </noscript>
-                  <Main>{children}</Main>
-                  <Footer onFeedbackClick={this.handleFeedbackToggle} />
-                </div>
-              </IconContext.Provider>
-            )}
-          </ClassNames>
-        )}
-      </StaticQuery>
-    );
-  }
+                <noscript>
+                  <p
+                    css={css`
+                      color: #f4f4f4;
+                      margin-bottom: 0;
+                      padding: 1rem;
+                      ${mq.sm(css`
+                        padding: 0;
+                        text-align: center;
+                      `)}
+                    `}
+                  >
+                    Please enable JavaScript for full site functionality.
+                  </p>
+                </noscript>
+                <Main>{children}</Main>
+                <Footer onFeedbackClick={handleFeedbackToggle} />
+              </div>
+            </IconContext.Provider>
+          )}
+        </ClassNames>
+      )}
+    </StaticQuery>
+  );
 }
 
 Layout.propTypes = {
