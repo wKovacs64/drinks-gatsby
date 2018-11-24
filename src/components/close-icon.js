3f0db0012b8b3cf0fac72ebc3aa95887263e3af8
Justin Hall <justin.r.hall@gmail.com>
2018-11-23 22:35:46 -0700

    Add a feedback form (#14)

---
+++ src/components/close-icon.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const CloseIcon = props => (
+  <svg
+    fill="currentColor"
+    viewBox="0 0 490 490"
+    width="1em"
+    height="1em"
+    {...props}
+  >
+    <title>Close</title>
+    <path d="M11.387 490L245 255.832 478.613 490l10.826-10.826-233.63-234.178 233.63-234.185L478.613 0 245 234.161 11.387 0 .561 10.811l233.63 234.185L.561 479.174z" />
+  </svg>
+);
+
+export default CloseIcon;
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
 import styled, { css } from 'react-emotion';
 import { FaGithub } from 'react-icons/fa';
 import mq from '../utils/mq';
@@ -16,63 +17,123 @@ const FooterLink = styled.a`
   }
 `;
 
-const Footer = () => (
-  <footer
-    className={css`
-      display: grid;
-      grid-gap: 2rem;
-      align-items: center;
-      justify-items: center;
-      background-color: #111111;
-      color: #cccccc;
-      padding: 1rem;
-      font-size: 1rem;
-      ${mq.sm(css`
-        font-size: 1.25rem;
-        grid-template-columns: repeat(2, minmax(max-content, 1fr));
-        justify-items: stretch;
-      `)};
-      ${mq.md(css`
-        padding: 2rem;
-      `)};
-    `}
-  >
-    <span>
-      Built with{' '}
-      <span role="img" aria-label="booze">
-        🥃
-      </span>{' '}
-      using{' '}
-      <FooterLink href="https://www.gatsbyjs.org/" rel="noopener noreferrer">
-        Gatsby
-      </FooterLink>
-      ,{' '}
-      <FooterLink href="https://www.contentful.com/" rel="noopener noreferrer">
-        Contentful
-      </FooterLink>
-      , and{' '}
-      <FooterLink href="https://emotion.sh/" rel="noopener noreferrer">
-        Emotion
-      </FooterLink>
-    </span>
-    <a
+const Footer = ({ onFeedbackClick }) => (
+  <Fragment>
+    <footer
       className={css`
-        color: currentColor;
-        text-decoration: none;
-        transition: color 0.3s ease;
-        &:hover {
-          color: #f4f4f4;
-        }
+        display: grid;
+        grid-gap: 2rem;
+        grid-template-areas:
+          'built-with built-with'
+          'feedback view-source';
+        align-items: center;
+        justify-content: stretch;
+        background-color: #111111;
+        color: #cccccc;
+        padding: 1rem;
+        font-size: 1rem;
         ${mq.sm(css`
-          justify-self: end;
+          justify-content: center;
+          font-size: 1.25rem;
+        `)};
+        ${mq.md(css`
+          padding: 2rem;
         `)};
       `}
-      href="https://github.com/wKovacs64/drinks"
-      rel="noopener noreferrer"
     >
-      <FaGithub aria-label="View source on GitHub" size={32} />
-    </a>
-  </footer>
+      <span
+        className={css`
+          grid-area: built-with;
+        `}
+      >
+        Built with{' '}
+        <span role="img" aria-label="booze">
+          🥃
+        </span>{' '}
+        using{' '}
+        <FooterLink href="https://www.gatsbyjs.org/" rel="noopener noreferrer">
+          Gatsby
+        </FooterLink>
+        ,{' '}
+        <FooterLink
+          href="https://www.contentful.com/"
+          rel="noopener noreferrer"
+        >
+          Contentful
+        </FooterLink>
+        , and{' '}
+        <FooterLink href="https://emotion.sh/" rel="noopener noreferrer">
+          Emotion
+        </FooterLink>
+      </span>
+      <button
+        type="button"
+        onClick={onFeedbackClick}
+        className={css`
+          grid-area: feedback;
+          justify-self: start;
+          display: inline-block;
+          color: currentColor;
+          background-color: transparent;
+          border: 1px solid #cccccc;
+          cursor: pointer;
+          font-size: 1rem;
+          padding: 0.5rem;
+          white-space: nowrap;
+          transition: color 0.3s ease, border-color 0.3s ease;
+          &:hover {
+            color: #f4f4f4;
+            border-color: #f4f4f4;
+          }
+          &:active {
+            color: currentColor;
+            border-color: #cccccc;
+          }
+          ${mq.sm(css`
+            padding: 1rem;
+          `)}
+        `}
+      >
+        Send Feedback
+      </button>
+      <a
+        className={css`
+          grid-area: view-source;
+          justify-self: end;
+          color: currentColor;
+          text-decoration: none;
+          transition: color 0.3s ease;
+          &:hover {
+            color: #f4f4f4;
+          }
+        `}
+        href="https://github.com/wKovacs64/drinks"
+        rel="noopener noreferrer"
+      >
+        <FaGithub aria-label="View source on GitHub" size={32} />
+      </a>
+    </footer>
+    {/* Help for the Netlify bots as the real form is rendered on-demand */}
+    <form
+      data-netlify="true"
+      data-netlify-honeypot="terminator"
+      action="/feedback-sent/"
+      name="drinks-feedback"
+      hidden
+    >
+      <input type="text" name="name" />
+      <input type="email" name="email" />
+      <textarea name="message" />
+    </form>
+  </Fragment>
 );
+
+Footer.propTypes = {
+  onFeedbackClick: PropTypes.func,
+};
+
+Footer.defaultProps = {
+  onFeedbackClick: () => {},
+};
 
 export default Footer;
