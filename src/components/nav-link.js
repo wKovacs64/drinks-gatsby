6ad94daa441802bba56accf83949063e530caa22
Justin Hall <justin.r.hall@gmail.com>
2018-12-08 12:59:07 -0700

    Apply hover styles to focused nav links

--- src/components/nav-link.js
+++ src/components/nav-link.js
@@ -5,8 +5,10 @@ export default styled(Link)`
   padding-bottom: 0.25rem;
   color: currentColor;
   text-decoration: none;
-  transition: color 0.3s ease, border-color 0.3s ease, box-shadow 0.3s ease;
-  &:hover {
+  transition: 0.3s ease;
+  transition-property: color, border-color, box-shadow;
+  &:hover,
+  &:focus {
     color: #f4f4f4;
     border-color: #f4f4f4;
     box-shadow: inset 0 -2px 0 0 #f4f4f4;
