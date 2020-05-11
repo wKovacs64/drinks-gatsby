fd2ddba70eb8b16279f80cab8ea51b3c2cd8a257
Justin Hall <justin.r.hall@gmail.com>
2020-05-11 09:57:09 -0600

    fix(a11y): restructure navigation/breadcrumbs for accessibility (#103)

--- src/components/nav.js
+++ src/components/nav.js
@@ -16,7 +16,15 @@ const Nav = ({ children, ...props }) => (
     `}
     {...props}
   >
-    {children}
+    <ul
+      css={css`
+        margin: 0;
+        padding: 0;
+        list-style-type: none;
+      `}
+    >
+      {children}
+    </ul>
   </nav>
 );
 
