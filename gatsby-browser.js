1cc5c2719b3fea546a738987fa1298290f252ae3
Justin Hall <justin.r.hall@gmail.com>
2020-12-24 11:43:46 -0700

    chore(deps): update fontsource to v4

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,6 +3,6 @@
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
-import 'fontsource-source-sans-pro/latin.css';
+import '@fontsource/source-sans-pro/latin.css';
 import '@wkovacs64/normalize.css';
 import '@reach/skip-nav/styles.css';
