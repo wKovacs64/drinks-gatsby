d22805f3aa526b314cb3628097940d033d9397a8
Justin Hall <justin.r.hall@gmail.com>
2021-03-01 18:15:10 -0700

    chore(lint): remove extraneous prettier configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['plugin:wkovacs64/react', 'prettier', 'prettier/react'],
+  extends: ['plugin:wkovacs64/react', 'prettier'],
   rules: {
     'react/react-in-jsx-scope': 'off',
   },
