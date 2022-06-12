6c17887cf34edfdfe97bb25d2b21274b638ed2cb
Justin Hall <justin.r.hall@gmail.com>
2022-06-12 11:09:26 -0600

    chore: type tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   plugins: [],
